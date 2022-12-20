776993c5148b7d656047b974921ccaff684b3f3c
Magib <112185392+mageeeb@users.noreply.github.com>
2022-12-20 16:18:26 +0100

    20-12Figma

---
+++ sitefigma/index.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($_GET['p'])) {
+
+
+    switch ($_GET['p']) {
+        case 'ecole':
+            include "pages/ecole.php";
+            break;
+        case 'open':
+            include 'pages/openhuiz.php';
+            break;
+        case 'prog':
+            include 'pages/programme.php';
+            break;
+        case 'studio':
+            include 'pages/studiobronz.php';
+            break;
+        case 'propos':
+            include 'pages/propos_de_bronz.php';
+            break;
+        case 'visit':
+            include 'pages/votre_visite.php';
+            break;
+        default:
+            include "pages/404.php";
+    }
+} else {
+
+    include_once "pages/homepages.php";
+}
---
+++ sitefigma/pages/homepages.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
+    <title>Site de Figma</title>
+</head>
+
+<body>
+
+    <header class="header">
+        <div class="div">
+            <img src="/images/logo Bronks.png" alt="logo">
+        </div>
+        <div class="images">
+            <?php
+            include "menu.php"
+            ?>
+            <div class="image">
+                <img src="/images/photos home/Rectangle 4.png" alt="">
+                <img src="/images/photos home/Rectangle 5.png" alt="">
+                <img src="/images/photos home/Rectangle 6.png" alt="">
+                <img src="/images/photos home/Rectangle 7.png" alt="">
+                <img src="/images/photos home/Rectangle 8.png" alt="">
+                <img src="/images/photos home/Rectangle 9.png" alt="">
+            </div>
+        </div>
+        <div class="text">
+            <p>Restez informé !</p>
+            <p>abonnez-vous à votre bulletin</p>
+            <div id="button">
+                <button class="button">
+                    <p>Abonez-vous</p>
+                </button>
+            </div>
+        </div>
+
+        <div class="logo">
+            <img src="/images/logos partenaires/vlaanderen-logo.jpg" alt="">
+            <img src="/images/logos partenaires/logo_vbx.png" alt="">
+            <img src="/images/logos partenaires/bruzz_logo.png" alt="">
+        </div>
+        <div class="footer">
+            <p>Rue du marché 15-171000 Bruxelles</p>
+            <p>joe 219 99 21einfo@bronks.be</p>
+            <p>horaires d’ouverture de la réception : mercredi de 13h00 à 17h30</p>
+            <p>@design by pixel - 2022</p>
+        </div>
+
+    </header>
+
+
+
+</html>
