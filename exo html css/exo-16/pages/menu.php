791a6e9c071448ca463751dfb24c294b1672f179
Magib <112185392+mageeeb@users.noreply.github.com>
2023-01-16 15:33:06 +0100

    html16/01

---
+++ exo html css/exo-16/pages/menu.php
@@ -0,0 +1,14 @@
+<nav class="navmenu">
+    <div class="menu">
+        <ul>
+            <li><a class="nav-link active" aria-current="page" href="./">Home</a></li>
+            <li><a href="?p=p1">page1</a></li>
+            <li><a href="?p=p2">page2</a></li>
+            <li><a href="?p=p">page3</a></li>
+            <li><a href="?p=p4">page4</a></li>
+
+
+        </ul>
+    </div>
+
+</nav>
